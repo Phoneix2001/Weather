d7353a091cde0b275873b8f9d3aa15f07a2f0830
arpit <arpit.babharoia@networkon.io>
2025-04-18 00:58:51 +0530

    improvement

--- lib/services/api_calling/app_url/app_url.dart
+++ lib/services/api_calling/app_url/app_url.dart
@@ -5,5 +5,5 @@
 sealed class AppUrl {
   AppUrl._();
  static String weather = "https://api.openweathermap.org/data/2.5/weather?";
- static String apiKey = 'ed8cf1a0b12a3677f40ecbee2222e1af'; 
+ static String apiKey = '*************'; 
 }
